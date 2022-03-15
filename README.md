9806c78407c9e7f14a2b222d77886a9e2ec4aca8
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-03-15 10:14:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Examen_U22
+ Examen de la unidad 2 de Aplicaciones Mociles
---
+++ app/src/main/java/equipo5/dahir/examen_u2/MainActivity.kt
@@ -0,0 +1,11 @@
+package equipo5.dahir.examen_u2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
