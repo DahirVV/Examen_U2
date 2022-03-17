bc99109a7ef0f6164695e234a1f02182c12cca4d
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-03-17 09:11:02 -0600

    avance un poco

---
+++ app/src/main/java/equipo5/dahir/examen_u2/Menu_Activity.kt
@@ -0,0 +1,11 @@
+package equipo5.dahir.examen_u2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
